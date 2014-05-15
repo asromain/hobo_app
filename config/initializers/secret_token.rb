4f9429287ad64c80bfba0e7d9ab582595779d26f
Asromain <mesprojetsinfo@gmail.com>
2014-05-15 14:21:56 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppHobo::Application.config.secret_key_base = 'b3709ba51c98d22a88952170f8cd7622f8c22993342a2a9e75861407c966d1fa0f544f142bf2ab03caede4efae0ac9453d2f7ce5c1d8e8e7450fc35b4f7a14d5'
